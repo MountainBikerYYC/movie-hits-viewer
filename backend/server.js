deef4c175ce72d952ff11043362d4184701e614a
MountainBikerYYC <ywang9696@gmail.com>
2023-01-25 23:22:31 -0700

    removed dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+// const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
 const connectDB = require("./config/db");
