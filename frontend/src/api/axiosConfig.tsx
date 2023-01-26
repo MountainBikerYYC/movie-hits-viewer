d2cfc64ba30bc8f59fbabae710b0e09d0ae4409b
MountainBikerYYC <ywang9696@gmail.com>
2023-01-25 23:12:42 -0700

    change URL path in axios

--- frontend/src/api/axiosConfig.tsx
+++ frontend/src/api/axiosConfig.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "https://9c96-103-106-239-104.ap.ngrok.io",
-  baseURL: "http://localhost:5000",
+  //   baseURL: "http://localhost:5000",
+  baseURL: "/",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
